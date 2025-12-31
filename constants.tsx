d4734743a372656ac863d6ab84b2f247aa0ac49c
Paul Muner <paulmuner@gmail.com>
2025-12-30 22:20:40 -0800

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -5,70 +5,70 @@ export const INITIAL_APPS: AppLink[] = [
     id: '1',
     title: 'Team Properties',
     description: 'Active and off-market Properties',
-    url: 'https://example.com',
-    icon: 'MapPin'
+    url: 'https://iconrealty.softr.app/',
+    icon: 'MapPinned'
   },
   {
     id: '2',
     title: 'Signs Manager',
     description: 'Check-in and check-out signs',
-    url: 'https://example.com',
-    icon: 'Briefcase'
+    url: 'https://signmanager.emergent.host/login',
+    icon: 'Baseline'
   },
   {
     id: '3',
     title: 'Submission Zone',
     description: 'New Escrows and More',
-    url: 'https://example.com',
-    icon: 'FileText'
+    url: 'https://sites.google.com/mcluregroup.com/hub/submit-here?',
+    icon: 'CopyCheck'
   },
   {
     id: '4',
     title: 'My eXp Apps',
     description: 'eXp Dashboard',
-    url: 'https://example.com',
-    icon: 'LayoutGrid'
+    url: 'https://exprealty.okta.com/app/UserHome',
+    icon: 'FileText'
   },
   {
     id: '5',
     title: 'CMA Generator',
     description: 'Create comparable market reports',
-    url: 'https://example.com',
-    icon: 'Activity'
+    url: 'https://www.narrpr.com/home',
+    icon: 'BarChart3'
   },
   {
     id: '6',
     title: 'Housing Report',
     description: 'Market Insights',
-    url: 'https://example.com',
+    url: 'https://sites.google.com/mcluregroup.com/hub/housing-report?',
     icon: 'House'
   },
   {
     id: '7',
     title: 'Celebrity Agent',
     description: 'Listing generation',
-    url: 'https://example.com',
+    url: 'https://backoffice.celebrityagent.com/login?',
     icon: 'Share2'
   },
   {
     id: '8',
     title: 'My Transactions',
     description: 'Closed transactions',
-    url: 'https://example.com',
-    icon: 'DollarSign'
+    url: 'https://my.exprealty.com/agent/agent-detail-report',
+    icon: 'CircleDollarSign'
   },
   {
     id: '9',
     title: 'The HUB',
     description: 'Central Team Site',
-    url: 'https://example.com',
+    url: 'https://sites.google.com/mcluregroup.com/hub/home',
     icon: 'Settings'
   },
   {
     id: '10',
     title: 'OC TAX',
     description: 'OC Tax info',
-    url: 'https://example.com',
-    icon: 'CreditCard'
+    url: 'https://mello.ocgov.com/#!/search',
+    icon: 'Banknote'
   }
 ];
